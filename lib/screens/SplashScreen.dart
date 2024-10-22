1b7949f6b1e5630a4e0fafc3a88ed90e1ecc2a79
pakdhen-unsur <ramad6130@gmail.com>
2024-10-22 15:02:43 +0700

    mobile Splah Screen pertemuan ke-4

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Splashscreen extends StatelessWidget {
+  const Splashscreen({super.key});
+
+  get fx000000 => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0XFFF0F3F5),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Lottie.network(
+                  'https://lottie.host/956e1e4f-8c98-4206-ae82-50dd50161d69/dtw01aXDDE.json'),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    height: 300,
+                  ),
+                  Text(
+                    'ASLABTIF',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                  Text(
+                    'TRAVEL',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff000000),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
